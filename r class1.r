4920b4fbb50e30347fdd6acd00171121b2eaa8e3
Ahanna Herbert <79004771+ahannaherbert@users.noreply.github.com>
2021-02-25 16:07:55 +0530

    Create r class1.r

---
+++ r class1.r
@@ -0,0 +1,39 @@
+a = 2
+b <- 19
+a == b
+a = b
+(y = x ** 4)
+(x <- 2 + 3)
+(y = x ** 4)
+5 * 9 -> a
+a + 3
+nummy <- c(2,3,4)
+nummy_int <- c(1L,2L,3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric(nummy)
+is.numeric(nummy_int)
+is.double(nummy)
+logicals <- c(TRUE,F,TRUE,F)
+is.logicals <- c(2,2,3)
+money_in_chars <- c("29","28","27")
+typeof(money_in_chars)
+money_money <- as.numeric(money_in_chars)
+money_money
+typeof(money_in_chars)
+typeof(money_money)
+as.logical(money_in_chars)
+money_logical <- as.logical(money_money)
+money_logical
+as.logical(c(29,28,27))
+month.abb
+month.abb[6]
+month.abb[5:10]
+1:6
+seq(5,10,2)
+month.abb[7,8,12]
+month.abb[c(7,8,12)]
+days <- c("mon","tue","wed")
+days
+phone <- c("samsumg","iphone","moto")
+phone
